e7124f0909a94e7fdaaa267aff21f6e3edade03d
bawejahritik <bawejahritik@gmail.com>
2023-09-19 13:35:19 -0400

    add typewriter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,11 @@
 import {motion} from 'framer-motion';
-import {styles} from '../style.js'
+import {styles} from '../style.js';
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
+
+  
+
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
@@ -10,7 +14,8 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
         <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Hritik</span></h1>
+        
+            <h1 className={`${styles.heroHeadText} text-white`}><Typewriter onInit={(typewriter) => { typewriter.typeString("Hi, I'm <span style='color: #915eff'>Hritik</span>").pauseFor(2000).start(); }}/></h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop softwares, user <br className='sm:block hidden'/> interfaces and web applications. <br className='sm:block hidden'/>Working towards making technology accessible</p>
           </div>
           
@@ -25,6 +30,7 @@ const Hero = () => {
               transition={{
                 duration: 1.5,
                 repeat: Infinity, 
+                
                 repeatType: 'loop'
               }}
               className='w-3 h-3 rounded-full bg-secondary mb-1'
